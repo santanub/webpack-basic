ff4612b56829bae5dd3edacc484bc2b579da214c
Santanu Bhattacharya <sbhattacharya@kreeti.com>
2017-12-25 17:58:52 +0530

    html + image load using html-loader and use clean webpack plugin to clean up flder for each prod build

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 var path = require('path');
 //var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
 
 var extractPlugin = new ExtractTextPlugin({
     filename: 'basic.css'
 });
+
+var htmlWebpackPlugin = new HtmlWebpackPlugin({
+    template: "index.html"
+})
 
 module.exports = {
     entry: "./src/js/app.js",
@@ -35,10 +41,30 @@ module.exports = {
                         "sass-loader"
                     ]
                 })
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.(jpg|png)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'src/distributor/img/',
+                            publicPath: 'src/distributor/img/'
+                        }
+                    }
+                ]
             }
+
         ]
     },
     plugins: [
-        extractPlugin
+        extractPlugin,
+        htmlWebpackPlugin,
+        new CleanWebpackPlugin(["src/distributor"])
     ]
 }
